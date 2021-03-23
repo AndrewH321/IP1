22b805c749053fc90567788d7c72513cc89991fa
gambit931 <yusaf_safdar@hotmail.com>
2021-03-23 09:47:40 +0000

    Add files via upload

---
+++ IP1/src/Test1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author yusaf
+ */
+public class Test1 {
+ static void CustomerID() {
+           iujki-=wqae
+        }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       CustomerID();
+       TUtorID();
+       
+        
+    
+    public int CustomerID {
+    
+    
+}
+}
+
+    
